6240e427fb69979ce1a92545908d1fc34c036249
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2023-02-17 19:44:56 +0100

    [CHANGED] port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3100, () => {
+server.listen(3200, () => {
   console.log("JSON Server is running");
 });
